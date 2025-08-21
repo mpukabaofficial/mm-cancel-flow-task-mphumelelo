aedc37d86ea71369bace351d0d1e76b414f829e4
Mphumelelo Khaba <mpukaba.official@gmail.com>
2025-08-21 17:36:57 -0400

    improved the cancel modal file and refractored it

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,17 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
-const Button = ({ children }: { children: ReactNode }) => {
+const Button = ({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}) => {
   return (
-    <button className="flex w-full h-8 sm:h-10 py-2 sm:py-3 px-4 sm:px-6 justify-center items-center gap-[10px] self-stretch rounded-lg border-2 border-gray-warm-300 text-gray-warm-700 text-center text-xs sm:text-sm font-semibold leading-[100%] tracking-[-0.32px]">
+    <button
+      onClick={onClick}
+      className="hover:bg-gray-50 transition-colors ease-in-out duration-150 flex w-full h-8 sm:h-10 py-2 sm:py-3 px-4 sm:px-6 justify-center items-center gap-[10px] self-stretch rounded-lg border-2 border-gray-warm-300 text-gray-warm-700 text-center text-xs sm:text-sm font-semibold leading-[100%] tracking-[-0.32px]"
+    >
       {children}
     </button>
   );
